5e76284d64c1f66722da1407136748084a2d195c
anthonyygpz <anthonyygpz@gmail.com>
2026-06-20 22:40:27 -0700

    chore: reorder imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:game_tv/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:game_tv/app.dart';
 
 void main() {
   runApp(const ProviderScope(child: App()));
